c24bf900d6869e0f1ea17400e17311bb0cbc9c43
Ron Garrison <ron.garrison.ca@gmail.com>
2020-04-04 13:21:58 -0700

    support multiple videos per name

--- get_urls.py
+++ get_urls.py
@@ -10,14 +10,15 @@ try:
 
     with open("url_list.txt", "w") as url_file:
         for entry in data:
-            url = entry["screensavers"][0]["videoURL"]
-            name = entry["name"].replace(" ", "-")
-            ftype = url.rsplit("/")[-1].split(".")[1]
-            local_file = f"{name}.{ftype}"
-            try:
-                r = requests.get(url, allow_redirects=True, verify=False)
-                open(local_file, "wb").write(r.content)
-            except Exception as e:
-                print(name, e)
+            for idx, track in enumerate(entry["screensavers"]):
+                url = track["videoURL"]
+                name = entry["name"].replace(" ", "-")
+                ftype = url.rsplit("/")[-1].split(".")[1]
+                local_file = f"{name}-{idx}.{ftype}"
+                try:
+                    r = requests.get(url, allow_redirects=True, verify=False)
+                    open(local_file, "wb").write(r.content)
+                except Exception as e:
+                    print(name, e)
 except Exception as e:
     print(e)
